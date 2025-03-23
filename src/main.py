a27f1406baa1ae8257a5ace5aa017c945f6485bd
fagocitoergosum <fakeaddress@fakemail.fake>
2025-03-23 21:48:09 +0100

    implement steps to deploy on github pages: basepath as an argument for generate_pages_recursive, docs as public folder and build script

--- src/main.py
+++ src/main.py
@@ -3,20 +3,15 @@ from htmlnode import HtmlNode, LeafNode, ParentNode
 from md_html_utils import *
 from page_generation_utils import *
 from pprint import pprint
-'''
-md = """# This is a heading
+import sys
 
-This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
+basepath = "/"
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
 
-1. This is the first list item in a list block
-2. This is a list item
-3. This is another list item"""
+#sostituiamo docs a public perché la directory di default dalla quale github servirà il sito è la directory docs del branch main
+#copy_static_public("static", "public")
+#generate_pages_recursive(basepath, "content", "template.html", "public")
 
-blocks = markdown_to_blocks(md)
-#print(*blocks, sep="\n")
-
-print(block_to_block_type(blocks[2]))
-'''
-#print(os.listdir("."))
-copy_static_public("static", "public")
-generate_pages_recursive("content", "template.html", "public")
+copy_static_public("static", "docs")
+generate_pages_recursive(basepath, "content", "template.html", "docs")
--- src/page_generation_utils.py
+++ src/page_generation_utils.py
@@ -29,25 +29,28 @@ def extract_title(markdown):
             return line.lstrip("#").strip()
     raise Exception("No title found, it must be an h1 line")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(template_path) as template_file:
         page = template_file.read()
     with open(from_path) as content_file:
         content_string = content_file.read()
     page = page.replace("{{ Title }}", extract_title(content_string)).replace("{{ Content }}", markdown_to_html_node(content_string).to_html())
+    page = page.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')#serve per deployare su github pages, perché il root path del sito partirà da una subdirectory
     dest_dir_path = os.path.dirname(dest_path)
     if not os.path.exists(dest_dir_path):
         os.makedirs(dest_dir_path)
     with open(dest_path, "w") as dest_file:
         dest_file.write(page)
 
-def generate_pages_recursive(content_dir_path, template_path, dest_dir_path):
+def generate_pages_recursive(basepath, content_dir_path, template_path, dest_dir_path):
+    #basepath la usiamo solo per sostituire la root standart negli attributi href e src nei tag html
+    #per i path che usiamo per generare il sito non c'è bisogno perché la generazione avviene in locale
     for name in os.listdir(content_dir_path):
         src_path = os.path.join(content_dir_path, name)
         if os.path.isdir(src_path):
             dst_path = os.path.join(dest_dir_path, name)
-            generate_pages_recursive(src_path, template_path, dst_path)
+            generate_pages_recursive(basepath, src_path, template_path, dst_path)
         else:
             dst_path = os.path.join(dest_dir_path, f"{name.split(".")[0]}.html")
-            generate_page(src_path, template_path, dst_path)
+            generate_page(basepath, src_path, template_path, dst_path)
